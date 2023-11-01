028de1fdc2961c8960169309815accb4ac10039b
Ayda2234 <202032009633@etu.univ-chlef.dz>
2023-11-01 13:39:18 +0100

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_url = "https://api.quotable.io/Random";
+ async function getQuote(url){
+    const response = await fetch(url)
+    var data = await response.json();
+
+    quote.innerHTML = data.content;
+    author.innerHTML = data.author;}
+    getQuote(api_url);
